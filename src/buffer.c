22513532d3d6c8c336b5bf2b32335fad9931b28d
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-11-26 13:39:25 +0500

    chore: clang-format style added

--- src/buffer.c
+++ src/buffer.c
@@ -16,7 +16,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct buffer *buffer_create() {
+struct buffer *buffer_create()
+{
   struct buffer *buf = calloc(sizeof(struct buffer), 1);
   buf->data = calloc(BUFFER_REALLOC_AMOUNT, 1);
   buf->len = 0;
@@ -24,19 +25,22 @@ struct buffer *buffer_create() {
   return buf;
 }
 
-void buffer_extend(struct buffer *buffer, size_t size) {
+void buffer_extend(struct buffer *buffer, size_t size)
+{
   buffer->data = realloc(buffer->data, buffer->msize + size);
   buffer->msize += size;
 }
 
-void buffer_need(struct buffer *buffer, size_t size) {
+void buffer_need(struct buffer *buffer, size_t size)
+{
   if (buffer->msize <= (buffer->len + size)) {
     size += BUFFER_REALLOC_AMOUNT;
     buffer_extend(buffer, size);
   }
 }
 
-void buffer_printf(struct buffer *buffer, const char *fmt, ...) {
+void buffer_printf(struct buffer *buffer, const char *fmt, ...)
+{
   va_list args;
   va_start(args, fmt);
   int index = buffer->len;
@@ -49,7 +53,8 @@ void buffer_printf(struct buffer *buffer, const char *fmt, ...) {
   va_end(args);
 }
 
-void buffer_printf_no_terminator(struct buffer *buffer, const char *fmt, ...) {
+void buffer_printf_no_terminator(struct buffer *buffer, const char *fmt, ...)
+{
   va_list args;
   va_start(args, fmt);
   int index = buffer->len;
@@ -62,7 +67,8 @@ void buffer_printf_no_terminator(struct buffer *buffer, const char *fmt, ...) {
   va_end(args);
 }
 
-void buffer_write(struct buffer *buffer, char c) {
+void buffer_write(struct buffer *buffer, char c)
+{
   buffer_need(buffer, sizeof(char));
 
   buffer->data[buffer->len] = c;
@@ -71,7 +77,8 @@ void buffer_write(struct buffer *buffer, char c) {
 
 void *buffer_ptr(struct buffer *buffer) { return buffer->data; }
 
-char buffer_read(struct buffer *buffer) {
+char buffer_read(struct buffer *buffer)
+{
   if (buffer->rindex >= buffer->len) {
     return -1;
   }
@@ -80,7 +87,8 @@ char buffer_read(struct buffer *buffer) {
   return c;
 }
 
-char buffer_peek(struct buffer *buffer) {
+char buffer_peek(struct buffer *buffer)
+{
   if (buffer->rindex >= buffer->len) {
     return -1;
   }
@@ -88,7 +96,8 @@ char buffer_peek(struct buffer *buffer) {
   return c;
 }
 
-void buffer_free(struct buffer *buffer) {
+void buffer_free(struct buffer *buffer)
+{
   free(buffer->data);
   free(buffer);
 }
